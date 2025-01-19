57ee3e2bef7a38e330a81252269ae77624f7b9af
ZvoneST <zstipic1989@gmail.com>
2025-01-19 11:45:03 +0100

    refactor - use env vars

--- src/utils/source_configs.py
+++ src/utils/source_configs.py
@@ -1,41 +1,41 @@
 import logging
+import os
+
 import yaml
 
 from src.utils.paths import *
+from sentinelhub import SHConfig
 
 __all__ = [
-    'open_config_file',
+    'sentinel_configs',
     'database_conn_configs'
 ]
 
 logger = logging.getLogger(__name__)
 
 
-def open_config_file(file_path: str):
-    with open(file_path, 'r') as file:
-        try:
-            logger.info(f'Getting config file for sentinel creds')
-            configs_file = yaml.load(file, Loader=yaml.FullLoader)
-            return configs_file
-        except FileNotFoundError as e:
-            logger.error(f'File not found at path: {file_path}')
-            raise e
-        except yaml.YAMLError as e:
-            logger.error(f'Error parsing config file: {e}')
-            raise e
-        except Exception as e:
-            logger.error(f'An unexpected error occurred: {e}')
-            raise e
+def sentinel_configs() -> SHConfig:
+    configs =  {'instance_id': os.environ['SH_INSTANCE'],
+                'sh_client_id': os.environ['SH_CLIENT_ID'],
+                'sh_client_secret': os.environ['SH_CLIENT_SECRET']}
+    sat_configs = SHConfig(**configs)
+    logger.info(f"Loaded sentinelhub credentials")
+    return sat_configs
 
 
-def database_conn_configs():
-    with open(SOURCE_CONFIGS, "r") as db:
+def database_conn_configs(db: str):
+    if db == 'src':
+        db_configs = SOURCE_CONFIGS
+    else:
+        db_configs = DESTINATION_CONFIGS
+
+    with open(db_configs, "r") as db:
         try:
             db_config = yaml.load(db, Loader=yaml.FullLoader)
             logger.info(f'Database connection protocol defined')
             return db_config
         except FileNotFoundError as e:
-            logger.error(f'File not found at path: {SOURCE_CONFIGS}')
+            logger.error(f'File not found at path: {db_configs}')
             raise e
         except yaml.YAMLError as e:
             logger.error(f'Error loading db config YAML file: {e}')
