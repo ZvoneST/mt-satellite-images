b19f227edd45d9c5b0369f855fc2ec707549a3ca
ZvoneST <zstipic1989@gmail.com>
2025-01-19 11:44:44 +0100

    refactor - removing paths

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -2,14 +2,14 @@ import os
 
 __all__ = [
     'SOURCE_CONFIGS',
-    'SENTINEL_CONFIGS',
-    'IMAGES_DIR_REMOTE'
+    'DESTINATION_CONFIGS',
+    'IMAGES_DIR'
 ]
 
 _ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 _CONFIGS_DIR = os.path.join(_ROOT_DIR, 'configs')
 
 SOURCE_CONFIGS = os.path.join(_CONFIGS_DIR, 'db_source.yml')
-SENTINEL_CONFIGS = os.path.join(_CONFIGS_DIR, 'sentinel.yml')
+DESTINATION_CONFIGS = os.path.join(_CONFIGS_DIR, 'db_destination.yml')
 
-IMAGES_DIR_REMOTE = '/home/cladmin/Downloads/DEM/'
+IMAGES_DIR = '/home/zvone/sentinel_images/landing/'
