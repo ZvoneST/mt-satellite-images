823c77aa26d450ebb87f8a99c7d00a152932584c
ZvoneST <zstipic1989@gmail.com>
2025-01-19 11:44:16 +0100

    refactor - db conn include dest

--- src/utils/connector.py
+++ src/utils/connector.py
@@ -12,58 +12,21 @@ logging.basicConfig(
 
 
 class DatabaseConnector:
-    """
-    This class facilitates creating a connection to a database using SQLAlchemy.
 
-    Attributes:
-        logger (Logger): Logger object for logging messages.
-        db_config (dict): Database configuration loaded from a YAML file.
-    """
-
-    def __init__(self):
-        """
-        Initializes the database configurations.
-
-        Raises:
-            FileNotFoundError: If the YAML file specified by STAGING_DB_PATH is not found.
-            yaml.YAMLError: If there is an error loading the database configuration YAML file.
-            Exception: If an unexpected error occurs.
-        """
+    def __init__(self, db_label: str):
         self.logger = logging.getLogger(__name__)
-        self.db_config = database_conn_configs()
+        self.db_config = database_conn_configs(db=db_label)
 
     def get_url(self) -> str:
-        """
-        Create database connection string url.
-
-        Returns:
-        -------
-        connection_string : str
-            A database connection string url.
-        """
         connection_string = f"{self.db_config['driver_name']}://" \
                             f"{os.environ['DB_USERNAME']}:" \
                             f"{os.environ['DB_PASSWORD']}@" \
-                            f"{self.db_config['host']}:" \
+                            f"{os.environ['HOST']}:" \
                             f"{self.db_config['port']}/" \
-                            f"{self.db_config['database']}?" \
-                            f"{self.db_config['additional_options']}"
+                            f"{self.db_config['database']}?"
         return connection_string
 
     def get_engine(self) -> Engine:
-        """
-        Create a SQLAlchemy engine object using the database configuration.
-
-        Returns:
-        -------
-        engine : Engine
-            A SQLAlchemy engine object that can be used to interact with the database.
-
-        Raises:
-        ------
-        Exception
-            If there is an error creating the engine, this function raises an exception with the error message.
-        """
         try:
             engine = create_engine(url=self.get_url())
             self.logger.info('Created database engine.')
@@ -73,12 +36,6 @@ class DatabaseConnector:
             raise e
 
     def get_session(self) -> Session:
-        """
-        Creates a SQLAlchemy session object for performing database operations.
-
-        Returns:
-            object: SQLAlchemy session object.
-        """
         engine = self.get_engine()
         Session = sessionmaker(bind=engine)
         self.logger.info('Database connection session created.')
